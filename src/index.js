8aca945373f9bc94b291a6678477aafb4287635f
username <wnt925@gmail.com>
2019-10-22 12:06:21 -0600

    Add all

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
-import $ from 'jquery'
-
+// import $ from 'jquery'
+//
+const axios = require("axios");
 $(document).ready(() => {
-  // have fun!
+  getData(get_url)
+  postData(post_url)
+
 })
+
+
+const get_url = "https://wordwatch-api.herokuapp.com/api/v1/top_word";
+const getData = async url => {
+  try {
+    const response = await axios.get(get_url);
+    const data = response.data.word;
+    $(".top-word").append(Object.keys(data));
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const url = "https://wordwatch-api.herokuapp.com/api/v1/words";
+const postData = async url => {
+  axios.post(url, {
+    word: 'Fred',
+  })
+  $("text-area").append(Object.keys(word));
+
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+};
+
+
+
+postData(url)
